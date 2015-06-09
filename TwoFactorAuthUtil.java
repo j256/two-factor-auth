4c356870153518535cc84aef0802d5131b5755fc
Gray <gray.github@mailnull.com>
2015-06-09 15:36:14 -0400

    Create TwoFactorAuthUtil.java

---
+++ TwoFactorAuthUtil.java
@@ -0,0 +1,194 @@
+package com.j256.common.utils;
+
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Two factor Java implementation for the Time-based One-Time Password (TOTP) algorithm
+ */
+public class TwoFactorAuthUtil {
+
+        /** default time-step which is part of the spec, 30 seconds is default */
+        static final int TIME_STEP_SECONDS = 30;
+
+        private final Random random = new Random();
+
+        // here to not have to generate a Mac instance each time
+        private final ThreadLocal<Mac> macThreadLocal = new ThreadLocal<Mac>() {
+                @Override
+                protected Mac initialValue() {
+                        try {
+                                return Mac.getInstance("HmacSHA1");
+                        } catch (NoSuchAlgorithmException e) {
+                                throw new RuntimeException(e);
+                        }
+                }
+        };
+
+        /**
+         * Generate a secret key in base32 format (A-Z2-7)
+         */
+        public String generateBase32Secret() {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 16; i++) {
+                        int val = random.nextInt(32);
+                        if (val < 26) {
+                                sb.append((char) ('A' + val));
+                        } else {
+                                sb.append((char) ('2' + (val - 26)));
+                        }
+                }
+                return sb.toString();
+        }
+
+        /**
+         * Return the current number to be checked from user input. This can be compared against user input.
+         * 
+         * For more details of this magic algorithm, see:
+         * http://en.wikipedia.org/wiki/Time-based_One-time_Password_Algorithm
+         */
+        public String generateCurrentNumber(String secret) throws GeneralSecurityException {
+
+                byte[] key = decodeBase32(secret);
+
+                byte[] data = new byte[8];
+                long value = System.currentTimeMillis() / 1000 / TIME_STEP_SECONDS;
+                for (int i = 7; value > 0; i--) {
+                        data[i] = (byte) (value & 0xFF);
+                        value >>= 8;
+                }
+
+                // encrypt the data with the key and return the SHA1 of it in hex
+                SecretKeySpec signKey = new SecretKeySpec(key, "HmacSHA1");
+                Mac mac = macThreadLocal.get();
+                mac.init(signKey);
+                byte[] hash = mac.doFinal(data);
+
+                // take the 4 least significant bits from the encrypted string as an offset
+                int offset = hash[hash.length - 1] & 0xF;
+
+                // We're using a long because Java hasn't got unsigned int.
+                long truncatedHash = 0;
+                for (int i = offset; i < offset + 4; ++i) {
+                        truncatedHash <<= 8;
+                        // get the 4 bytes at the offset
+                        truncatedHash |= (hash[i] & 0xFF);
+                }
+                // cut off the top bit
+                truncatedHash &= 0x7FFFFFFF;
+
+                // the token is then the last 6 digits in the number
+                truncatedHash %= 1000000;
+
+                // make sure it is 0 prefixed
+                return String.format("%06d", truncatedHash);
+        }
+
+        /**
+         * Return the QR image url thanks to Google. This can be shown to the user and scanned by the authenticator program
+         * as an easy way to enter the secret.
+         */
+        public String qrImageUrl(String keyId, String secret) {
+                StringBuilder sb = new StringBuilder(128);
+                sb.append("https://chart.googleapis.com/chart?"
+                                + "chs=200x200&amp;cht=qr&amp;chl=200x200&amp;chld=M|0&amp;cht=qr&amp;chl=");
+                sb.append("otpauth://totp/").append(keyId).append("%3Fsecret%3D").append(secret);
+                return sb.toString();
+        }
+
+        /**
+         * Little decode base-32 method. We could use Apache Codec but I didn't want to have the dependency just for this
+         * decode method.
+         */
+        private byte[] decodeBase32(String str) {
+                // each base-32 character encodes 5 bits
+                int numBytes = ((str.length() * 5) + 4) / 8;
+                byte[] result = new byte[numBytes];
+                int resultIndex = 0;
+                int which = 0;
+                int working = 0;
+                for (int i = 0; i < str.length(); i++) {
+                        char ch = str.charAt(i);
+                        int val;
+                        if (ch >= 'a' && ch <= 'z') {
+                                val = ch - 'a';
+                        } else if (ch >= 'A' && ch <= 'Z') {
+                                val = ch - 'A';
+                        } else if (ch >= '2' && ch <= '7') {
+                                val = 26 + (ch - '2');
+                        } else {
+                                throw new IllegalArgumentException("Invalid base-32 character: " + ch);
+                        }
+                        /*
+                         * There are probably better ways to do this but this seemed the most straightforward.
+                         */
+                        switch (which) {
+                                case 0 :
+                                        // all 5 bits is top 5 bits
+                                        working = (val & 0x1F) << 3;
+                                        which = 1;
+                                        break;
+                                case 1 :
+                                        // top 3 bits is lower 3 bits
+                                        working |= (val & 0x1C) >> 2;
+                                        result[resultIndex++] = (byte) working;
+                                        // lower 2 bits is upper 2 bits
+                                        working = (val & 0x03) << 6;
+                                        which = 2;
+                                        break;
+                                case 2 :
+                                        // all 5 bits is mid 5 bits
+                                        working |= (val & 0x1F) << 1;
+                                        which = 3;
+                                        break;
+                                case 3 :
+                                        // top 1 bit is lowest 1 bit
+                                        working |= (val & 0x10) >> 4;
+                                        result[resultIndex++] = (byte) working;
+                                        // lower 4 bits is top 4 bits
+                                        working = (val & 0x0F) << 4;
+                                        which = 4;
+                                        break;
+                                case 4 :
+                                        // top 4 bits is lowest 4 bits
+                                        working |= (val & 0x1E) >> 1;
+                                        result[resultIndex++] = (byte) working;
+                                        // lower 1 bit is top 1 bit
+                                        working = (val & 0x01) << 7;
+                                        which = 5;
+                                        break;
+                                case 5 :
+                                        // all 5 bits is mid 5 bits
+                                        working |= (val & 0x1F) << 2;
+                                        which = 6;
+                                        break;
+                                case 6 :
+                                        // top 2 bits is lowest 2 bits
+                                        working |= (val & 0x18) >> 3;
+                                        result[resultIndex++] = (byte) working;
+                                        // lower 3 bits of byte 6 is top 3 bits
+                                        working = (val & 0x07) << 5;
+                                        which = 7;
+                                        break;
+                                case 7 :
+                                        // all 5 bits is lower 5 bits
+                                        working |= (val & 0x1F);
+                                        result[resultIndex++] = (byte) working;
+                                        which = 0;
+                                        break;
+                        }
+                }
+                if (which != 0) {
+                        result[resultIndex++] = (byte) working;
+                }
+                if (resultIndex != result.length) {
+                        result = Arrays.copyOf(result, resultIndex);
+                }
+                return result;
+        }
+}
