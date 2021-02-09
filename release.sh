171d527a02e8f2b82f865993789c405a354f50e8
Gray Watson <gray.github@mailnull.com>
2021-02-09 14:40:33 -0500

    cp

--- release.sh
+++ release.sh
@@ -6,14 +6,21 @@
 LIBRARY="two-factor-auth"
 LOCAL_DIR="$HOME/svn/local/$LIBRARY"
 
+git status | head -1 | fgrep master > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    /bin/echo "Should be on master branch."
+    git status | head -1
+    exit 1
+fi
+
 #############################################################
 # check ChangeLog
 
 head -1 src/main/javadoc/doc-files/changelog.txt | fgrep '?' > /dev/null 2>&1
 if [ $? -ne 1 ]; then
-	echo "No question-marks (?) can be in the ChangeLog top line."
-	head -1 src/main/javadoc/doc-files/changelog.txt
-	exit 1
+    /bin/echo "No question-marks (?) can be in the ChangeLog top line."
+    head -1 src/main/javadoc/doc-files/changelog.txt
+    exit 1
 fi
 
 #############################################################
@@ -22,9 +29,9 @@ fi
 cd $LOCAL_DIR
 git status | grep 'nothing to commit'
 if [ $? -ne 0 ]; then
-	/bin/echo "Files not checked-in"
-	git status
-	exit 1
+    /bin/echo "Files not checked-in"
+    git status
+    exit 1
 fi
 
 #############################################################
@@ -38,7 +45,7 @@ fi
 
 #############################################################
 
-release=`grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1 -d\-`
+release=$(grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1 -d\-)
 
 /bin/echo ""
 /bin/echo ""
@@ -47,24 +54,39 @@ release=`grep version pom.xml | grep SNAPSHOT | head -1 | cut -f2 -d\> | cut -f1
 /bin/echo -n "Enter release number [$release]: "
 read rel
 if [ "$rel" != "" ]; then
-	release=$rel
+    release=$rel
 fi
+
+# remove the local and remote tag if any
+tag="$LIBRARY-$release"
+git tag -d $tag 2> /dev/null
+git push --delete origin $tag 2> /dev/null
 
 #############################################################
 # check docs:
 
 cd $LOCAL_DIR
-ver=`head -1 src/main/javadoc/doc-files/changelog.txt | cut -f1 -d:`
+ver=$(head -1 src/main/javadoc/doc-files/changelog.txt | cut -f1 -d:)
 if [ "$release" != "$ver" ]; then
-	echo "Change log top line version seems wrong:"
-	head -1 src/main/javadoc/doc-files/changelog.txt
-	exit 1
+    /bin/echo "Change log top line version seems wrong:"
+    head -1 src/main/javadoc/doc-files/changelog.txt
+    exit 1
+fi
+
+if [ -r "src/main/doc/$LIBRARY.texi" ]; then
+    ver=$(grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi | cut -f3 -d' ')
+    if [ "$release" != "$ver" ]; then
+	/bin/echo "$LIBRARY.texi version seems wrong:"
+	grep "^@set ${LIBRARY}_version" src/main/doc/$LIBRARY.texi
+	/bin/echo -n "Press control-c to quit otherwise return.  [ok] "
+	read cont
+    fi
 fi
 
 grep -q $release README.md
 if [ $? != 0 ]; then
-	echo "Could not find $release in README.md"
-	exit 1
+    /bin/echo "Could not find $release in README.md"
+    exit 1
 fi
 
 #############################################################
@@ -107,8 +129,8 @@ read cont
 if [ "$cont" = "" -o "$cont" = "y" ]; then
     cd $LOCAL_DIR
     mvn -P st release:clean || exit 1
-    mvn $GPG_ARGS -P st release:prepare || exit 1
-    mvn $GPG_ARGS -P st release:perform || exit 1
+    mvn $GPG_ARGS -P st release:prepare || ( /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1 )
+    mvn $GPG_ARGS -P st release:perform || ( /bin/echo "Maybe use mvn release:rollback to rollback"; exit 1 )
 
     /bin/echo ""
     /bin/echo ""
